269f37024e52f5c2a2cac721c34c8ad536575406
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-05-08 13:05:33 +0200

    Add quarterly

--- src/rotation.js
+++ src/rotation.js
@@ -9,13 +9,18 @@ module.exports = function rotation (d = new Date(), opts = {}) {
   // between 1 and 31
   if (d.getUTCDate() === 1) {
     // between 0 and 11
-    if (d.getUTCMonth() === 0) {
-      ret.push('yearly', d.getUTCFullYear())
+    // if (d.getUTCMonth() === 0) {
+    //   ret.push('yearly', d.getUTCFullYear())
+    // } else
+    if ((d.getUTCMonth() + 1) % 4 === 0) {
+      ret.push('quarterly', d.getUTCFullYear(), Math.floor(d.getUTCMonth() / 4))
     } else {
       ret.push('monthly', zeropad(d.getUTCMonth() + 1, 2), utcMonths[d.getUTCMonth()])
     }
+  } else if (d.getUTCDate() % 7 === 0) {
+    ret.push('weekly', Math.floor(d.getUTCDate() / 7))
   } else {
-    ret.push('weekly', d.getUTCDay(), utcWeekdays[d.getUTCDay()])
+    ret.push('daily', d.getUTCDay(), utcWeekdays[d.getUTCDay()])
   }
 
   return ret.join('-')
