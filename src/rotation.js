00ec99bb81c5d75cd984d1a382360a4abdfab56d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-05-08 10:41:49 +0200

    Ditch zeropad

--- src/rotation.js
+++ src/rotation.js
@@ -1,8 +1,6 @@
 const utcWeekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 const utcMonths = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
-const zeropad = require('./zeropad')
-
 module.exports = function rotation (d = new Date(), opts = {}) {
   let ret = []
 
@@ -10,7 +8,7 @@ module.exports = function rotation (d = new Date(), opts = {}) {
   if (d.getUTCDate() === 1) {
     // between 0 and 11
     if (d.getUTCMonth() === 0) {
-      ret.push(`yearly-${zeropad(d.getUTCFullYear(), 4)}`)
+      ret.push(`yearly-${d.getUTCFullYear()}`)
     } else {
       ret.push(`monthly-${utcMonths[d.getUTCMonth()]}`)
     }
--- src/zeropad.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (s, size = 0) {
-  while (s.length < (size)) {
-    s = `0${s}`
-  }
-  return s
-}
