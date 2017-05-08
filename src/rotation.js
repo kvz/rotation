e9c69b107fe9f481152fd30122500b2f1c69af5e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-05-08 12:47:59 +0200

    simpler test

--- src/rotation.js
+++ src/rotation.js
@@ -8,7 +8,7 @@ module.exports = function rotation (d = new Date(), opts = {}) {
   if (d.getUTCDate() === 1) {
     // between 0 and 11
     if (d.getUTCMonth() === 0) {
-      ret.push(`yearly-${d.getUTCFullYear()}`)
+      ret.push('yearly', d.getUTCFullYear())
     } else {
       ret.push(`monthly-${utcMonths[d.getUTCMonth()]}`)
     }
--- src/rotation.test.js
+++ src/rotation.test.js
@@ -3,18 +3,13 @@ const isodate = require('./isodate')
 
 describe('rotation', () => {
   test('should match snapshot for every day from 2017-01-01 to 2019-01-01', () => {
-    let msgs = []
+    const files = {}
     for (let i = 1483232461; i <= 1546304461; i = i + 86400) {
-      let d = new Date(i * 1000)
-      let res = rotation(d)
-      let id = isodate(d)
-      let msg = `${id}: ${res}`
-      msgs.push(msg)
+      let dateJs      = new Date(i * 1000)
+      let filename    = rotation(dateJs)
+      let dateIso     = isodate(dateJs)
+      files[filename] = dateIso
     }
-    expect(msgs).toMatchSnapshot()
-    // expect(rotation(new Date('2017-05-01T15:01:01'))).toBe('saturday')
-    // expect(rotation(new Date('2017-05-06T15:01:01'))).toBe('saturday')
-    // expect(rotation(new Date('2017-05-07T15:01:01'))).toBe('sunday')
-    // expect(rotation(new Date('2017-05-08T15:01:01'))).toBe('monday')
+    expect(files).toMatchSnapshot()
   })
 })
